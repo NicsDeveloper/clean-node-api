c2d767c76739eb247569e6ea947045ec5b8931d3
Nícolas Nascimento <nicsdesenvolvedor@gmail.com>
2024-09-01 00:35:26 -0300

    feat: add package engine data

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,5 @@
 export default {
-  mongoUrl: 'mongodb+srv://nicsdesenvolvedor:%3CD20HfgRY9TtbUs3Q%3E@clean-node-db.iw47f.mongodb.net/',
+  mongoUrl: 'mongodb+srv://nicsdesenvolvedor:D20HfgRY9TtbUs3Q@clean-node-db.iw47f.mongodb.net/',
   port: process.env.PORT || 5050,
   jwtSecret: process.env.JWT_SECRET || 'tj670==5H'
 }
