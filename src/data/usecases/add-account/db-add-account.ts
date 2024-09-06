6d68d57093eb96d669cb728ef3f3d854caf43f88
Nícolas Nascimento <nicsdesenvolvedor@gmail.com>
2024-09-06 02:09:13 -0300

    chore: remove src/data from git and eslint ignore

--- src/data/usecases/add-account/db-add-account.ts
+++ src/data/usecases/add-account/db-add-account.ts
@@ -4,7 +4,7 @@ export class DbAddAccount implements AddAccount {
   constructor (
     private readonly hasher: Hasher,
     private readonly addAccountRepository: AddAccountRepository
-  ) {}
+  ) { }
 
   async add (accountData: AddAccountModel): Promise<AccountModel> {
     const hashedPassword = await this.hasher.hash(accountData.password)
