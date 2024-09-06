801f8b4f9da62c021ffbeffd610d88ff4044af05
Nícolas Nascimento <nicsdesenvolvedor@gmail.com>
2024-09-06 00:40:13 -0300

    feat: add debug and docker

--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -5,8 +5,11 @@ import path from 'path'
 export default async (app: Express): Promise<void> => {
   const router = Router()
   app.use('/api', router)
+
+  // Ler os arquivos da pasta 'routes'
   readdirSync(path.join(__dirname, '..', 'routes')).map(async file => {
-    if (!file.includes('.test.')) {
+    // Ignorar arquivos de teste e arquivos .map
+    if (!file.includes('.test.') && (file.endsWith('.ts') || file.endsWith('.js'))) {
       (await import(`../routes/${file}`)).default(router)
     }
   })
