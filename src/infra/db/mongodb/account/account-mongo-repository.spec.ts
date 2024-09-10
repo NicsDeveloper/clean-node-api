95736af50923df626ea7fd7a35a13a6f25d8456d
Nícolas Nascimento <nicsdesenvolvedor@gmail.com>
2024-09-10 05:11:35 -0300

    test: ensure AccountMongoRepository will not find an account if find account by token fails

--- src/infra/db/mongodb/account/account-mongo-repository.spec.ts
+++ src/infra/db/mongodb/account/account-mongo-repository.spec.ts
@@ -131,5 +131,13 @@ describe('Account Mongo Repository', () => {
       expect(account.email).toBe('any_email@mail.com')
       expect(account.password).toBe('any_password')
     })
+
+    it('Should return null if load by token fails', async () => {
+      const sut = makeSut()
+
+      const account = await sut.loadByToken('any_token')
+
+      expect(account).toBeFalsy()
+    })
   })
 })
