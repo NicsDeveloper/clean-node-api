79c4b45fe4eabdadb3c791489f67ea6fe63e65b0
Nícolas Nascimento <nicsdesenvolvedor@gmail.com>
2024-09-10 04:12:07 -0300

    testt: ensure JwtAdapter returns a decrypter value on verify success

--- src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
+++ src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
@@ -48,5 +48,12 @@ describe('Jwt Adapter', () => {
 
       expect(verifySpy).toHaveBeenCalledWith('any_token', 'secret')
     })
+
+    it('Should return a value on verify success', async () => {
+      const sut = makeSut()
+      const value = await sut.decrypt('token')
+
+      expect(value).toBe('any_value')
+    })
   })
 })
