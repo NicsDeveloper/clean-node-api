faba9773df811a6bf6441d0921cbc16a959dd3cc
Nícolas Nascimento <nicsdesenvolvedor@gmail.com>
2024-09-12 00:39:04 -0300

    feat: ensure SurveyMongoRepository load all surveys

--- src/infra/db/mongodb/survey/survey-mongo-repository.ts
+++ src/infra/db/mongodb/survey/survey-mongo-repository.ts
@@ -1,9 +1,23 @@
+import { type LoadSurveysRepository } from '../../../../data/protocols/db/survey/load-surveys-repository'
 import { type AddSurveyModel, type AddSurveyRepository } from '../../../../data/usecases/add-survey/db-add-survey-protocols'
+import { type SurveyModel } from '../../../../domain/models/survey'
 import { MongoHelper } from '../helper/mongo-helper'
 
-export class SurveyMongoRepository implements AddSurveyRepository {
+export class SurveyMongoRepository implements AddSurveyRepository, LoadSurveysRepository {
   async add (surveyData: AddSurveyModel): Promise<void> {
     const surveyCollection = await MongoHelper.getCollection('surveys')
     await surveyCollection.insertOne(surveyData)
+  }
+
+  async loadAll (): Promise<SurveyModel[]> {
+    const surveyCollection = await MongoHelper.getCollection('surveys')
+    const surveys = await surveyCollection.find().toArray()
+
+    return surveys.map(survey => ({
+      id: survey._id.toString(),
+      question: survey.question,
+      answers: survey.answers,
+      date: survey.date
+    }))
   }
 }
