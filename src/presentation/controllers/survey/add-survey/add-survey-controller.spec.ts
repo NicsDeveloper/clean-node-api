3a22f552cea4afb1988f6a4b2d37d5c998787365
Nícolas Nascimento <nicsdesenvolvedor@gmail.com>
2024-09-12 02:54:53 -0300

    refactor: change models to use type alias instead of interfaces

--- src/presentation/controllers/survey/add-survey/add-survey-controller.spec.ts
+++ src/presentation/controllers/survey/add-survey/add-survey-controller.spec.ts
@@ -24,7 +24,7 @@ const makeValidationStub = (): Validation => {
   return new ValidationStub()
 }
 
-interface SutTypes {
+type SutTypes = {
   sut: AddSurveyController
   validationStub: Validation
   addSurveyStub: AddSurvey
