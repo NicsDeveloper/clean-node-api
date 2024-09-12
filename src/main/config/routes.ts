31331f75aa6ae94446e835ed469f1e62b13a73fb
Nícolas Nascimento <nicsdesenvolvedor@gmail.com>
2024-09-12 03:10:39 -0300

    fix: make controller returns the error message instead of the error name

--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -6,9 +6,7 @@ export default async (app: Express): Promise<void> => {
   const router = Router()
   app.use('/api', router)
 
-  // Ler os arquivos da pasta 'routes'
   readdirSync(path.join(__dirname, '..', 'routes')).map(async file => {
-    // Ignorar arquivos de teste e arquivos .map
     if (!file.includes('.test.') && (file.endsWith('.ts') || file.endsWith('.js'))) {
       (await import(`../routes/${file}`)).default(router)
     }
